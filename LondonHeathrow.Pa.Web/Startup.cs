69633bc8a4fa6aeebd7c1abefa4923939be72023
peterschubert <pschub99@gmail.com>
2023-07-15 13:29:31 -0400

    convert to use startup.cs

---
+++ LondonHeathrow.Pa.Web/Startup.cs
@@ -0,0 +1,54 @@
+﻿using LondonHeathrow.Pa.Web.Data;
+using LondonHeathrow.Pa.Web.Hubs;
+using Microsoft.EntityFrameworkCore;
+namespace LondonHeathrow.Pa.Web;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddRazorPages();
+        services.AddSignalR();
+        services.AddResponseCompression(options =>
+        {
+            options.MimeTypes = new[] { "application/octet-stream" };
+        });
+
+        services.AddDbContext<PaDbContext>(options =>
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        });
+
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapRazorPages();
+            endpoints.MapHub<PaIotHub>("/pa-iot");
+        });
+    }
+}
+
